7e997ae69121a8699beddf7ffad41c8a989dfbe6
Yassin Nurmahomed <yassin791@gmail.com>
2012-11-07 21:43:54 -0200

    p check()

--- src/Maluco/Maluco.java
+++ src/Maluco/Maluco.java
@@ -19,10 +19,7 @@ public class Maluco{
 		
 			parser p = new parser(new Lexico(new FileReader(args[0])));
 			result = (Arquivo) p.parse().value;
-			if(p.erro > 0){
-				System.out.println("\nCorrija os erros sintáticos e tente outra vez\n");
-				System.exit(1);
-			}
+			p.check();
 			System.out.println(" Terminada a análise léxica e sintática \n Árvore resultante:\n" + result.toString("") + "\n");
 			System.out.println("\n Preenchendo a tabela de símbolos\n");
 			result.traverseTopDown(scopeChecker);
